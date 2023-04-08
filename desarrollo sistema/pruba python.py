0922206470bf542f646ce4b00762d3763559dc06
juanabello219 <128939966+juanabello219@users.noreply.github.com>
2023-04-08 15:09:01 -0500

    comit
    actualizaciones

--- desarrollo sistema/pruba python.py
+++ desarrollo sistema/pruba python.py
@@ -2,11 +2,7 @@ from os import system
 system ("cls")
 import os 
 import random
-
-#entradas
-#declaramos variables 
-RECTIFICADORES=["RNEC","#Votaciones2023"]
-VOTANTES={}
+VOTANTES=[]
 #CORREGIMIENTOS
 ZONAS_RURALES={1:"palenquillo",2:"puerto viejo",3:"mahoma",4:"la estacion",5:"cascajal",6:"el contento",7:"puerto mosquito"}
 #BARRIOS URBANOS
@@ -16,10 +12,8 @@ CANDIDATOS={1:"pepito perez",2:"juanito gonzales",3:"pedro picapiedra",4:"fernan
 #PUESTOS Y MESAS DE VOTACION
 PUESTO_URBANOS={"instituciion educativa rafael salazar":21,"institucion educativa promocion social":2}
 PUESTO_RURALES={"palenquillo":3,"puerto viejo":1,"mahoma":1,"la estacion":2,"cascajal":1,"el contento":2,"puerto mosquito":2}
-#declaramos las funciones 
-#esta funcion me permite registrar los votantes para las elecciones municipales
-def registro_votantes(VOTANTES,ZONAS_URBANA,ZONAS_RURALES,PUESTO_URBANOS,PUESTO_RURALES):
-    VOTANTES2={}
+def registro_votantes(VOTANTES):
+    VOTANTES2=[]
     ZONAS_RURALES={1:"palenquillo",2:"puerto viejo",3:"mahoma",4:"la estacion",5:"cascajal",6:"el contento",7:"calpulco nuevo",8:"puerto mosquito"}
     ZONAS_URBANA={1:"san jose",2:"araujo",3:"el carmen",4:"san martin",5:"el prado",6:"el comercio",7:"aceitunos",8:"san antonio",9:"capulco nuevo",10:"divino niño",11:"brisas del cesar"}
     PUESTO_URBANOS={"INSTITUCION EDUCATIVA RAFAEL SALAZAR":21,"INSTITUCION EDUCATIVA DE PROMOCIÓN SOCIAL":2}
@@ -34,16 +28,16 @@ def registro_votantes(VOTANTES,ZONAS_URBANA,ZONAS_RURALES,PUESTO_URBANOS,PUESTO_
             break
     if cedula in VOTANTES:
         print("su numero de documentos se encuentra en la base de datos por favor digite nuevamente")
+        VOTANTES2.append(cedula)
     else:
-        VOTANTES2["cedula"]=cedula
         nombre=str(input("ingrese su nombre:"))
         apellido=str(input("ingresa tu apellido:"))
-        VOTANTES2["nombre"]=nombre
-        VOTANTES2["apellido"]=apellido
+        VOTANTES2.append(nombre)
+        VOTANTES2.append(apellido)
         SELEC_ZONA=int(input("SELECIONA TU ZONA 1.ZONA URBANA /2.ZONA RURAL:"))
         if SELEC_ZONA >2:
             quit()
-        VOTANTES2["zonas:"]=SELEC_ZONA
+        VOTANTES2.append(SELEC_ZONA)
         if SELEC_ZONA == 1:
             print("tus zonas urbanas son:")
             for i in ZONAS_URBANA.items():
@@ -54,27 +48,27 @@ def registro_votantes(VOTANTES,ZONAS_URBANA,ZONAS_RURALES,PUESTO_URBANOS,PUESTO_
                 for i in  PUESTO_URBANOS.keys():
                     print(i)
                 puesto=input("seleccione la sede donde desea votar:")
-                VOTANTES2["PUESTO-VOTACION"]=puesto
+                VOTANTES2.append(puesto)
                 if puesto == "institucion educativa rafael salazar":
                     mesa=random.randrange(1,21)
-                    VOTANTES2["mesa"]=mesa
+                    VOTANTES2.append(mesa)
                     print(f"tu mesa de votacion es la mesa numero: {mesa}") 
                 elif puesto == "institucion educativa de promocion social":
                     mesa= random.randrange(1,3)
-                    VOTANTES2["mesa"]=mesa
+                    VOTANTES2.append(mesa)
                     print(f"tu mesa de votacion es la mesa numero: {mesa}")
         if SELEC_ZONA == 2:
             print("tus zonas rurales son:")
             for i in ZONAS_RURALES.items():
                 print(i)
             OP_SELECCION2=int(input("SELECCIONA TU ZONA:"))
-            VOTANTES2["zonas corregimiento"]=OP_SELECCION2
+            VOTANTES2.append(OP_SELECCION2)
             if OP_SELECCION2 <=7:
                 print("tus puestos de votacion son:")
                 for i in  PUESTO_RURALES.keys():
                     print(i)
             puesto=input("seleccione de nuevo corregimiento para obtener su puesto de votacion:")
-            VOTANTES2["lugar_votacion"]=puesto
+            VOTANTES2.append(puesto)
             if puesto == "palenquillo":
                 mesa=random.randrange(1,3)
                 print(f"tu mesa de votacion es la mesa numero: {mesa}")
@@ -96,21 +90,23 @@ def registro_votantes(VOTANTES,ZONAS_URBANA,ZONAS_RURALES,PUESTO_URBANOS,PUESTO_
             elif puesto == "puerto mosquito":
                 mesa=random.randrange(1,2)
                 print(f"tu mesa de votacion es la mesa numero: {mesa}")
-            VOTANTES2["mesa"]=mesa
+            VOTANTES2.append(mesa)
             VOTANTES.append(VOTANTES2)
         opcion=input("desea agregar otro votante al sistema de registro si/no: ")
         if opcion == "no":
-            print("sus datos fueron guardados correctamente",VOTANTES)
-    return (VOTANTES,ZONAS_URBANA,ZONAS_RURALES,PUESTO_URBANOS,PUESTO_RURALES)
+            print("sus datos fueron guardados correctamente",VOTANTES2)
+    return (VOTANTES)
 
-def sistema_votacion(VOTANTES,CANDIDATOS):
+
+def sistema_votacion(VOTANTES):
     VOTANTES=[]
     x=0
     CANDIDATOS={1:"PEPITO PEREZ",2:"JUANITO GONZALES",3:"PEDRO PICAPIEDRA",4:"CANTINFLAS",5:"voto en blanco"}
     cedula=int(input("ingrese la cedula del votante"))
     print("los datos del votante son: ")
-    for i,x in VOTANTES.items():
-        print(i,x)
+    for i in VOTANTES:
+        x+=0
+        print(x,i)
     while True:
         print("los candidatos a la alcadia municipal son: ")
         for i in CANDIDATOS.items():
@@ -129,30 +125,11 @@ def menu_registro_y_votaciones():
         print("******************************************************")
         opcion=int(input("que opcion deseas:"))
         if opcion == 1:
-            registro_votantes(VOTANTES,ZONAS_URBANA,ZONAS_RURALES,PUESTO_URBANOS,PUESTO_RURALES)
+            registro_votantes(VOTANTES)
         if opcion == 2:
             os.system("cls")
-            sistema_votacion(VOTANTES,CANDIDATOS)
-def menu():
-    while(True):
-        print("-----------------SISTEMA DE VOTACION-------------------|")
-        print("|                                                      |")                                                    
-        print("-------------------------------------------------------|")
-        print("|1.INICIE SESION..                                     |")
-        print("|------------------------------------------------------|")
-        op_cajero=int(input("elije la opcion:-->"))
-        if op_cajero == 1:
-            os.system("cls")
-            rectificador()
+            sistema_votacion(VOTANTES)
 
-def rectificador():
-    if input("ingrese su usuario:--->") != RECTIFICADORES[0] or input("ingrese la contraseña-->") != RECTIFICADORES[1] or print("sesion iniciada") or os.system("cls") or menu_registro_y_votaciones():
-        print("acceso denegado")or quit()
 
 if __name__=="__main__":
-    print("bienvenidos al sistema de registro y votaciones,para nosotros en un placer que nos hayas elegido como plataforma")
-    print("para estas nuevas elecciones municipales 2023")
-    print("                                           ")
-    input("presiona enter para continuar..:")
-    os.system("cls")
-    menu()
+    menu_registro_y_votaciones()
